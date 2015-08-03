3ba9712409f20991eb877d012b73e028cf7193e0
Babolin Giordano <babolin.giordano@gmail.com>
2015-08-03 14:54:00 +0200

    Initial commit

---
+++ app/src/main/java/com/android/babo/signalsfrommars/StoryActivity.java
@@ -0,0 +1,28 @@
+package com.android.babo.signalsfrommars;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class StoryActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra(getString(R.string.key_name));
+
+        if(name == null){
+            name = "Friend";
+        }
+
+
+
+    }
+
+}
